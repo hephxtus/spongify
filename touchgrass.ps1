911d9a979e12ae62d4b00476903da4da1a63961b
Joseph Hodges <50793031+hephxtus@users.noreply.github.com>
2022-10-07 02:01:49 +1300

    it just works

---
+++ touchgrass.ps1
@@ -0,0 +1,29 @@
+Param (
+	[Parameter(Mandatory=$True, Position=1)]
+	[string]$To_Invert,
+    
+    [switch]$Switch = $false
+)
+$isup = $True
+$new_string = $To_Invert.ToCharArray()
+foreach ($x in $new_string) {
+    $i = $To_Invert.IndexOf($x)
+    if($x -notlike " ") {
+        if($isup){
+            $new_string[$i] = "$x".toupper()
+
+        }
+        else{
+            $new_string[$i] = "$x".tolower()
+        }
+        $isup = !$isup
+    }
+    
+    Write-Output ($new_string[$i], $isup)
+
+
+    
+}
+$new_string = -join $new_string
+Write-Output $new_string
+
