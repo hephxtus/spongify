c18548476953555d8845fe290c4c80b7882db21a
Joseph Hodges <50793031+hephxtus@users.noreply.github.com>
2022-10-07 02:55:14 +1300

    Update touchgrass.ps1

--- touchgrass.ps1
+++ touchgrass.ps1
@@ -1,25 +1,20 @@
 Param (
 	[Parameter(Mandatory=$True, Position=1)]
-	[string]$To_Invert,
-    
-    [switch]$Switch = $false
-)
-$isup = $True
-$new_string = $To_Invert.ToCharArray()
-foreach ($x in $new_string) {
-    $i = $To_Invert.IndexOf($x)
-    if($x -notlike " ") {
-        if($isup){
-            $new_string[$i] = "$x".toupper()
+	[string]$To_Invert
+    )
 
-        }
-        else{
-            $new_string[$i] = "$x".tolower()
-        }
-        $isup = !$isup
+$isup = $True
+$new_string = ""
+foreach ($x in $To_Invert.ToCharArray()) {
+    if($isup) {
+        $new_string = $new_string + "$x".toupper()
+
     }
     
+    else {
+        $new_string = $new_string + "$x".tolower()
+    }
 }
-$new_string = -join $new_string
+
 Write-Output $new_string
 
